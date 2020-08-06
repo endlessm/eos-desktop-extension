d923222e6aa6fd2793ec133ea5fa2a99d40b85c1
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2020-08-06 16:59:18 -0300

    layout: Add overview clones to desktop
    Implement a new OverviewClone class that clones the
    search bar, and creates a fake AppDisplay.
    
    This follows the old approach of creating everything,
    and hiding the entire actor tree from picking.
    
    When the eos-panel extension is ready, we'll need to
    update this code to add the panel *after* the app grid.
    
    https://phabricator.endlessm.com/T30543

--- ui/layout.js
+++ ui/layout.js
@@ -16,9 +16,89 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
+const { Clutter, GObject, Shell, St } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const DesktopExtension = ExtensionUtils.getCurrentExtension();
+
+const AppDisplay = imports.ui.appDisplay;
+const AppDisplayOverrides = DesktopExtension.imports.ui.appDisplay;
+const LayoutManager = imports.ui.layout;
 const Main = imports.ui.main;
 
+const EOS_INACTIVE_GRID_OPACITY = 96;
+
 let startupPreparedId = 0;
+
+var OverviewClone = GObject.registerClass(
+class OverviewClone extends St.BoxLayout {
+    _init() {
+        super._init({
+            name: 'overview',
+            opacity: EOS_INACTIVE_GRID_OPACITY,
+            vertical: true,
+        });
+
+        Shell.util_set_hidden_from_pick(this, true);
+
+        this.add_constraint(new LayoutManager.MonitorConstraint({ primary: true }));
+
+        // Add a clone of the panel to the overview so spacing and such is
+        // automatic
+        const panelGhost = new St.Bin({
+            child: new Clutter.Clone({ source: Main.panel }),
+            reactive: false,
+            opacity: 0,
+        });
+        this.add_child(panelGhost);
+
+        const searchEntryClone = new Clutter.Clone({
+            source: Main.overview.searchEntry.get_parent(),
+            x_align: Clutter.ActorAlign.CENTER,
+        });
+        this.add_actor(searchEntryClone);
+
+        const appDisplayClone = new AppDisplay.AppDisplay();
+        AppDisplayOverrides.changeAppGridOrientation(
+            Clutter.Orientation.HORIZONTAL,
+            appDisplayClone);
+        this.add_child(appDisplayClone);
+
+        // Bind adjustments
+        const { appDisplay } = Main.overview.viewSelector;
+        appDisplay._scrollView.hscroll.adjustment.bind_property('value',
+            appDisplayClone._scrollView.hscroll.adjustment, 'value',
+            GObject.BindingFlags.SYNC_CREATE);
+        appDisplay._scrollView.vscroll.adjustment.bind_property('value',
+            appDisplayClone._scrollView.vscroll.adjustment, 'value',
+            GObject.BindingFlags.SYNC_CREATE);
+
+        // Added by this extension's ui/appDisplay.js file
+        this._desaturateEffect = new Clutter.DesaturateEffect({
+            name: 'endless-desaturate',
+            factor: 1.0,
+        });
+        this.add_effect(this._desaturateEffect);
+    }
+});
+
+const bgGroups = [
+    Main.layoutManager._backgroundGroup,
+    Main.overview._backgroundGroup,
+];
+
+function addAppGridClone() {
+    bgGroups.forEach(group => group.add_child(new OverviewClone()));
+}
+
+function removeAppGridClone() {
+    bgGroups.forEach(actor => {
+        for (const child of actor) {
+            if (child instanceof OverviewClone)
+                child.destroy();
+        }
+    });
+}
 
 function enable() {
     if (startupPreparedId === 0) {
@@ -27,6 +107,8 @@ function enable() {
                 Main.overview.show();
             });
     }
+
+    addAppGridClone();
 }
 
 function disable() {
@@ -34,4 +116,6 @@ function disable() {
         Main.layoutManager.disconnect(startupPreparedId);
         startupPreparedId = 0;
     }
+
+    removeAppGridClone();
 }
