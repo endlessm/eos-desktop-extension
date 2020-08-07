ae845466f77726bcd2d319ba75fe4162f909a0bf
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2020-08-07 11:27:47 -0300

    overview: Add background menu to overview
    The background menu is a fallback menu; right-clicking on
    app grid icons, the search entry, etc, takes precedence
    over it.
    
    https://phabricator.endlessm.com/T30543

--- ui/overview.js
+++ ui/overview.js
@@ -19,9 +19,39 @@
 const ExtensionUtils = imports.misc.extensionUtils;
 const DesktopExtension = ExtensionUtils.getCurrentExtension();
 
+const BackgroundMenu = imports.ui.backgroundMenu;
 const Main = imports.ui.main;
 const Overview = imports.ui.overview;
 const Utils = DesktopExtension.imports.utils;
+
+function addBackgroundMenu() {
+    const overviewActor = Main.overview._overview;
+
+    if (overviewActor._backgroundMenu)
+        return;
+
+    const oldActionList = overviewActor.get_actions();
+
+    BackgroundMenu.addBackgroundMenu(overviewActor, Main.layoutManager);
+
+    const newActionList = overviewActor.get_actions();
+    const intersection = newActionList.filter(a => !oldActionList.includes(a));
+    overviewActor._bgMenuClickAction = intersection[0];
+}
+
+function removeBackgroundMenu() {
+    const overviewActor = Main.overview._overview;
+
+    if (!overviewActor._backgroundMenu)
+        return;
+
+    overviewActor._backgroundMenu.destroy();
+    overviewActor._backgroundMenu = null;
+    overviewActor._backgroundManager = null;
+
+    overviewActor.remove_action(overviewActor._bgMenuClickAction);
+    overviewActor._bgMenuClickAction = null;
+}
 
 function enable() {
     Utils.override(Overview.Overview, '_shadeBackgrounds', function() {});
@@ -34,8 +64,12 @@ function enable() {
         background.content.brightness = 1.0;
         background.content.vignette_sharpness = 0.0;
     }
+
+    addBackgroundMenu();
 }
 
 function disable() {
     Utils.restore(Overview.Overview);
+
+    removeBackgroundMenu();
 }
